787719629a7d87db9e7acf9fd52697a2922d01d4
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-06-14 19:44:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister  
 
+  attr_accessor :total, :cash_register_with_discount
+
+  def initialize 
+    @cash_register_with_discount = cash_register_with_discount
+    @total = 0 
+  end
+
+
+end
